c3a64a571df2c425ddcd1305fe9be3bf496b9c39
Saif Ul Islam <saifulislam84210@gmail.com>
2022-05-23 16:25:34 +0500

    :package: feat: Version 0.0.10 -> 0.0.11
    Signed-off-by: Saif Ul Islam <saifulislam84210@gmail.com>

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ import shutil
 
 # Setup variables. Change as needed
 NAME = "pupiilcommon"
-VERSION = "0.0.10"
+VERSION = "0.0.11"
 AUTHOR = "Saif Ul Islam"
 AUTHOR_EMAIL = "saifulislam84210@gmail.com"
 LICENSE = "MIT"
